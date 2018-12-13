2908b03f519f73dea3cd52a1329f63b51ccc2f6f
Fan <fan@FandeMBP.lan>
2018-12-13 10:55:27 +0800

    增加饱和度分析

--- src/main/java/team/chess/Action/DecideMan.java
+++ src/main/java/team/chess/Action/DecideMan.java
@@ -2,6 +2,7 @@ package team.chess.Action;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import team.chess.POJO.ChessboardPOJO;
 import team.chess.POJO.NodePOJO;
 import team.chess.POJO.RelationPOJO;
 import team.chess.POJO.StepPOJO;
@@ -13,7 +14,7 @@ import java.util.List;
 public class DecideMan {
     private SqlUtil sqlUtil;
 
-    public NodePOJO Decide(NodePOJO nodeBegin) throws IOException {
+    public NodePOJO weightDecide(NodePOJO nodeBegin) throws IOException {
         SqlSessionFactory sqlSessionFactory = sqlUtil.getSqlSessionFactory();
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
@@ -48,7 +49,44 @@ public class DecideMan {
             }
         }
         Integer nodeEndId = relationDecide.getNodeEndId();
-        NodePOJO nodeEnd = sqlSession.selectOne("team.chess.Mapper.NodeMapper", nodeEndId);
+        NodePOJO nodeEnd = sqlSession.selectOne("team.chess.Mapper.NodeMapper.queryObject", nodeEndId);
+        sqlSession.close();
         return nodeEnd;
+    }
+
+    //饱和度分析
+    public Float filledLevelDecide(NodePOJO nodeBegin) throws IOException {
+        SqlSessionFactory sqlSessionFactory = sqlUtil.getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //获取total
+        Integer total = 0;
+        Integer chessBoardId = nodeBegin.getChessboardId();
+        ChessboardPOJO chessboardPOJO = sqlSession.selectOne("team.chess.Mapper.ChessboardMapper.queryObject", chessBoardId);
+        List<String> lines = chessboardPOJO.getLines();
+        Integer x = nodeBegin.getX();
+        Integer y = nodeBegin.getY();
+        if (x-2 >= 0 && y-2 >= 0 && lines.get(x-2).charAt(y-2) != '0') total++;
+        if (y-2 >= 0 && lines.get(x-1).charAt(y-2) != '0') total++;
+        if (x <= 14 && y-2 >=0 && lines.get(x).charAt(y-2) != '0') total++;
+        if (x-2 >= 0 && lines.get(x-2).charAt(y-1) != '0') total++;
+        if (x <= 14 && lines.get(x).charAt(y-1) != '0') total++;
+        if (x-2 >= 0 && y <= 14 && lines.get(x-2).charAt(y) != '0') total++;
+        if (y <= 14 && lines.get(x-1).charAt(y) != '0') total++;
+        if (x <= 14 && y <= 14 && lines.get(x).charAt(x) != '0') total++;
+
+        //获取known
+        Integer known = 0;
+        Integer nodeBeginId = nodeBegin.getId();
+        List<RelationPOJO> relationPOJOS = sqlSession.selectList("team.chess.Mapper.RelationMapper.queryList", nodeBeginId);
+        known = relationPOJOS.size();
+
+        //饱和度
+        Float filledLevel;
+        if (total == 0) filledLevel = Float.valueOf(0);
+        else  filledLevel = Float.valueOf(known / total);
+
+        sqlSession.close();
+        return filledLevel;
     }
 }
