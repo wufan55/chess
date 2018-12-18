64fc1943007ae8cf2f8faef8807ef3c731879eab
Fan <fan@FandeMacBook-Pro.local>
2018-12-18 10:29:47 +0800

    select方法测试成功

--- src/main/java/team/chess/Action/testAction.java
+++ src/main/java/team/chess/Action/testAction.java
@@ -1,14 +1,14 @@
 package team.chess.Action;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import team.chess.POJO.testPOJO;
+import team.chess.POJO.RelationPOJO;
 import team.chess.Util.SqlUtil;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class testAction {
     public static void main(String[] args) throws IOException {
@@ -17,8 +17,11 @@ public class testAction {
         //创建SqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
         //获取对象
-        testPOJO testPojo = sqlSession.selectOne("team.chess.Mapper.testMapper.queryObject", 1);
-        System.out.println(testPojo.getName());
+        RelationPOJO relationPOJO = new RelationPOJO();
+        relationPOJO.setNodeBeginId(1);
+        List<RelationPOJO> testPojo = sqlSession.selectList("team.chess.Mapper.RelationMapper.queryList", relationPOJO);
+        if (testPojo.size() != 0) relationPOJO = testPojo.get(0);
+        System.out.println(relationPOJO.getStepId());
         sqlSession.close();
     }
 }
